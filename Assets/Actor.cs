c997109660c1a65c30798e95a6f91bf61f265c72
Drew Gingerich <gingerich93@gmail.com>
2018-11-02 22:04:21 -0700

    Create example scripts and scene.

---
+++ Assets/Actor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D))]
+public class Actor : MonoBehaviour {
+
+	[SerializeField]
+	private Collider2D otherCollider;
+
+	private const float shellWidth = 0.001f;
+
+	private Rigidbody2D rb2d;
+	private Collider2D collider;
+	private RaycastHit2D[] hitBuffer = new RaycastHit2D[8];
+	private ContactFilter2D contactFilter;
+	
+	void Awake() {
+		rb2d = GetComponent<Rigidbody2D>();
+		collider = GetComponent<Collider2D>();
+
+		contactFilter = new ContactFilter2D();
+		contactFilter.useTriggers = false;
+		contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+	}
+
+	public void Move(Vector2 move) {
+		float distance = move.magnitude;
+		Vector2 direction = move.normalized;
+		float searchDistance = distance;
+
+		int count = rb2d.Cast(direction, hitBuffer, searchDistance);
+		if (count > 0) {
+			distance = hitBuffer[0].distance;
+		}
+
+		// transform.Translate((Vector3)direction * distance);
+		rb2d.MovePosition(rb2d.position + direction * distance);
+
+		ColliderDistance2D colDistance = collider.Distance(otherCollider);
+		Debug.Log(string.Format("Cast distance: {0}, Overlap distance: {1}", distance, colDistance.distance));
+	}
+}
---
+++ Assets/SimpleBallController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Actor))]
+public class SimpleBallController : MonoBehaviour {
+
+	private Actor actor;
+
+	void Awake() {
+		actor = GetComponent<Actor>();
+	}
+
+	void FixedUpdate() {
+		actor.Move(Vector2.down * Time.deltaTime);
+	}
+}
